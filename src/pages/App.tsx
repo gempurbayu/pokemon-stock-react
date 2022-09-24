4bd2aa98ba7de1d8bda0d77a713df4d5eecb4372
gempurbayu <aji123zz@gmail.com>
2022-09-25 01:04:42 +0700

    tailwind integration

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import logo from './assets/logo.svg';
+import logo from '../assets/logo.svg';
 import './App.css';
 
 function App() {
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
+        <p className="text-3xl font-bold underline">
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
         <a
